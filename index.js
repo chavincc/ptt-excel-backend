e361afdc7991cad8438dd09e8b98877e5050ccf5
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-06-14 00:51:10 +0700

    feat(app): serve static file from /public

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const excelRoutes = require('./src/routes/excel');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
