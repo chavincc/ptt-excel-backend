383e69052136a4899f02b729a30ac3d1f0e4a08b
chavincc <cc6chavin@gmail.com>
2020-06-17 17:36:09 +0700

    feat(auth): implement route protection middleware

---
+++ src/middleware/validator/auth.js
@@ -0,0 +1,23 @@
+const { check } = require('express-validator')
+const jwt = require('jsonwebtoken')
+
+const validateSignUp = [
+    check('email').isEmail(),
+    check('password').isLength({ min : 8 })
+]
+
+const protectRoute = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    try {
+        const data = await jwt.verify(token, process.env.JWT_SECRET)
+        req.id = data.id;
+        next();
+    } catch (error) {
+        res.status(401).send('unauthorized request');
+    }
+}
+
+module.exports = {
+    validateSignUp,
+    protectRoute
+}
