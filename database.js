18fea6974649ccd762fa76453387a33dbe4855ed
chavincc <cc6chavin@gmail.com>
2020-08-06 18:50:06 +0700

    reset repository
    new architect design cause changes in server logic

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const Mongoose = require('mongoose');
+const Mongoose = require("mongoose");
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -6,25 +6,6 @@ Mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-Mongoose.set('useCreateIndex', true);
+Mongoose.set("useCreateIndex", true);
 
-const UserSchema = new Mongoose.Schema(
-  {
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { collection: 'user' }
-);
-
-module.exports = {
-  User: Mongoose.model('User', UserSchema),
-};
+module.exports = {};
--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-
-const excelRoutes = require('./src/routes/excel');
-const authRoutes = require('./src/routes/auth');
-const { protectRoute } = require('./src/middleware/validator/auth');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_DOMAIN);
-  res.header('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_DOMAIN);
+  res.header("Access-Control-Allow-Credentials", true);
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-app.use('/excel', protectRoute, excelRoutes);
-app.use('/auth', authRoutes);
-app.get('/', (req, res, next) => {
-  res.send('ptt excel service server');
+app.get("/", (req, res, next) => {
+  res.send("ptt construction monitoring REST server");
 });
 
 const PORT = process.env.PORT || 3000;
