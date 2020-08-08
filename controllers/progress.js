4589a11dc667994b25b9049996b4dd08e89e3071
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-08-09 01:40:26 +0700

    feat(progress): query data on GET /progress

--- controllers/progress.js
+++ controllers/progress.js
@@ -1,8 +1,31 @@
 const database = require('../database');
+const { CONSTRUCTION_METHOD } = require('../types/method');
+const { PROVINCE } = require('../types/province');
 
-const queryProgress = (req, res) => {
-  const { province, method, top } = req.query;
-  res.send(`province=${province}, method=${method}, top=${top}`);
+const queryProgress = async (req, res) => {
+  let { province, method, top } = req.query;
+
+  top = parseInt(top);
+  if (!top) {
+    top = 1;
+  }
+  if (!method) {
+    top *= Object.keys(CONSTRUCTION_METHOD).length - 1;
+  }
+  if (!province) {
+    top *= Object.keys(PROVINCE).length;
+  }
+
+  const query = {
+    ...(province && { province: province }),
+    ...(method && { method: method }),
+  };
+
+  const result = await database.Progress.find(query)
+    .sort({ computedDate: -1 })
+    .limit(top);
+
+  res.json(result);
 };
 
 module.exports = {
